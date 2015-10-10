824eb6aef7ec693116316af601c378921128156a
Yan Zhao <zhaoyan1117@gmail.com>
2015-10-10 16:55:26 -0700

    Initial Commit

---
+++ UTCClock/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  UTCClock
+//
+//  Created by YAN ZHAO on 10/10/15.
+//  Copyright © 2015 Yan Zhao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+

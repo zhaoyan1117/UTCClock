db0338fd74e8e98fc3ec3196606a6443b0d4931e
Yan Zhao <zhaoyan1117@gmail.com>
2015-10-10 19:58:00 -0700

    init

--- UTCClock/AppDelegate.swift
+++ UTCClock/AppDelegate.swift
@@ -10,18 +10,49 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     @IBOutlet weak var window: NSWindow!
-
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        setUpTimer()
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    let pasteBoard = NSPasteboard.generalPasteboard()
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+    let formatter = NSDateFormatter()
+    let timeZone = "UTC"
+    
+    func setUpTimer() {
+        if let button = statusItem.button {
+            button.action = Selector("copyToClipboard:")
+            button.title = generateTimeStr()
+        }
+        
+        NSTimer.scheduledTimerWithTimeInterval(
+            1.0,
+            target: self,
+            selector: Selector("tick:"),
+            userInfo: nil,
+            repeats: true
+        )
+    }
+    
+    func tick(timer: NSTimer) {
+        statusItem.button?.title = generateTimeStr()
+    }
+    
+    func generateTimeStr() -> String {
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        formatter.timeZone = NSTimeZone(abbreviation: timeZone);
+        return formatter.stringFromDate(NSDate())
+    }
+    
+    func copyToClipboard(sender: NSStatusBarButton) {
+        pasteBoard.clearContents()
+        pasteBoard.writeObjects([sender.title])
+    }
 }
-
